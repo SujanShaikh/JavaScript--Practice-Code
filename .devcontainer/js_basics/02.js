7aff580c25166a14f4b1a9937cae38f81b115a1b
SujanShaikh <78723148+SujanShaikh@users.noreply.github.com>
2024-04-20 16:06:51 +0000

    commit

---
+++ .devcontainer/js_basics/02.js
@@ -0,0 +1,6 @@
+const accountId=1233345;
+console.log(accountId)
+let username='sujanshaikh'
+var name='Naykaa'
+let password;
+ console.table([username,accountId,name,password])
---
+++ .devcontainer/js_basics/03_datatype.js
@@ -0,0 +1,13 @@
+"use strict" //treat all js code as newer version
+
+//console.log(1+1)
+
+let name='sujan'
+let age=24
+let isloggedin=false
+
+
+//null => standalone value
+
+console.log(typeof(null))
+console.log(typeof(isloggedin))
